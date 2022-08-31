68ad5f7c291acb54a5a247a11106313305c0c412
Jennifer <goodwije@gmail.com>
2022-08-30 22:39:28 -0600

    updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const compression = require('compression');
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/budget';
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/financial-security';
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(express.static('public'));
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 // routes
